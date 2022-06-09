6b2d92872fe8ef43feec3550f3413eb951ff4a39
ramzeng <ramzeng@tencent.com>
2022-06-09 12:37:08 +0800

    feat: 增加 fake 方法

--- src/Ims.php
+++ src/Ims.php
@@ -3,6 +3,7 @@
 namespace Overtrue\LaravelQcloudContentAudit;
 
 use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Str;
 
 /**
  * @method static array check(string $contents)
@@ -14,5 +15,72 @@ class Ims extends Facade
     protected static function getFacadeAccessor(): string
     {
         return 'ims';
+    }
+
+    public static function fake(array $methods = ['check', 'validate']): void
+    {
+        foreach ($methods as $method) {
+            call_user_func([self::class, \sprintf('fake%s', Str::ucfirst($method))]);
+        }
+    }
+
+    public static function fakeValidate(): void
+    {
+        self::shouldReceive('validate')->withAnyArgs()->andReturnTrue();
+    }
+
+    public static function fakeCheck(): void
+    {
+        self::shouldReceive('check')->withAnyArgs()->andReturn([
+            'Response' => [
+                'RequestId' => 'a61237dd-c2a0-43e7-a3da-d27022d39ba7',
+                'DataId' => 'a61237dd-c2a0-43e7-a3da-d27022d39ba7',
+                'BizType' => 'test_1001',
+                'Suggestion' => 'Block',
+                'FileMD5' => '',
+                'Label' => 'Porn',
+                'SubLabel' => 'SexBehavior',
+                'Score' => 90,
+                'LabelResults' => [
+                    [
+                        'Scene' => 'Porn',
+                        'Suggestion' => 'Block',
+                        'Label' => 'Porn',
+                        'SubLabel' => 'SexBehavior',
+                        'Score' => 90,
+                        'Details' => [],
+                    ],
+                ],
+                'ObjectResults' => [
+                    [
+                        'Scene' => 'QrCode',
+                        'Suggestion' => 'Block',
+                        'Label' => 'Ad',
+                        'SubLabel' => '',
+                        'Score' => 100,
+                        'Names' => [
+                            'QRCODE',
+                        ],
+                        'Details' => [
+                            [
+                                'Id' => 0,
+                                'Name' => 'QRCODE',
+                                'Score' => 100,
+                                'Location' => [
+                                    'X' => 155.01746,
+                                    'Y' => 396.01746,
+                                    'Width' => 769.9824,
+                                    'Height' => 769.98254,
+                                    'Rotate' => 0,
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'OcrResults' => [],
+                'LibResults' => [],
+                'Extra' => '',
+            ],
+        ]);
     }
 }
